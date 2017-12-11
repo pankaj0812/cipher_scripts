f6ec6250d62f8f6bd677730fdebcc70a7fa78c10
pankaj0812 <singhpankajkumar65@gmail.com>
2017-12-11 23:40:01 +0530

    appending affineHacker.py

--- affineHacker.py
+++ affineHacker.py
@@ -4,7 +4,7 @@ import pyperclip, affineCipher, detectEnglish, cryptomath
 SILENT_MODE = False
 
 def main():
-	myMessage = """U&'<3dJ^Gjx'-3^MS'Sj0jxuj'G3'%j'<mMMjS'g{GjMMg9j{G'g"'gG'<3^MS'Sj<jguj'm'P^dm{'g{G3'%jMgjug{9'GPmG'gG'-m0'P^dm{LU'5&Mm{'_^xg{9"""
+	myMessage = """U&'<3dJ^Gjx'-3^MS'Sj0jxuj'G3'%j'<mMMjS'g{GjMMg9j{G'g"'gG'<3^MS'Sj<jguj'm'P^dm{'g{G3'%jMgjug{9'GPmG'gG'-m0'P^dm{LU'5&Mm{'_^xg{9 """"
 	hackedMessage = hackAffine(myMessage)
 
 	if hackedMessage != None:
